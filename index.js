9cb6666f37180ef84f8d2eeeaa941b618f7eabcc
ZackarySantana <ZackZackYack@gmail.com>
2020-06-19 21:33:26 -0400

    Added default implementation of tilegame

---
+++ index.js
@@ -0,0 +1,52 @@
+// Libraries
+import React from 'react';
+import { Provider } from 'react-redux';
+import styled from 'styled-components';
+
+// Redux
+import store from './redux/store';
+
+// Components
+
+// Styles
+import './styles.scss';
+
+const Background = styled.div`
+	width: 100vw;
+	height: 100vh;
+
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+const GameBox = styled.div`
+	width: 80vw;
+	height: 80vh;
+	z-index: 1;
+
+	background-color: black;
+	border: 2px gray solid;
+`;
+
+export default (props) => {
+	console.log(props)
+	return (
+		<div id="tilegame-page">
+			<Background>
+				<GameBox>{/* Do game lol test */}</GameBox>
+			</Background>
+		</div>
+	);
+}
+
+export function tilegame(props) {
+	console.log(props)
+	return (
+		<div id="tilegame-page">
+			<Background>
+				<GameBox>{/* Do game lol test */}</GameBox>
+			</Background>
+		</div>
+	);
+}
