559d02f4cf0a2a2c708e4028a580a8d42749865d
Zackary Santana <zackzackyack@gmail.com>
2020-12-13 12:37:15 -0500

    Adding menu to tilegame

---
+++ menu/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {withRouter} from 'react-router';
+
+function Menu(props) {
+    const {history} = props;
+
+    return (
+        <div>
+            <button onClick={(event) => {
+                    history.push("/")
+                }}
+            />
+        </div>
+    )
+}
+
+export default withRouter(Menu);
